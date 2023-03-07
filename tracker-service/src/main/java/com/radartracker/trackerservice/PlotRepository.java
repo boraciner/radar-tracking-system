a5b8b7281bf0561944ac8c7a8bc58ba88e863a46
Bora Ciner <bora@ciner.de>
2023-03-07 21:14:35 +0100

    tracker-service is added

---
+++ tracker-service/src/main/java/com/radartracker/trackerservice/PlotRepository.java
@@ -0,0 +1,7 @@
+package com.radartracker.trackerservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlotRepository extends JpaRepository<Plot, Long>{
+
+}
---
+++ tracker-service/src/test/java/com/radartracker/trackerservice/TrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radartracker.trackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
